b4ffcfa4e55966ac43aa3b15216bb0ca625dbbb1
Pedro Giampietro <pedrogiampietro@hotmail.com>
2025-10-22 00:05:19 -0300

    send config

--- config.php
+++ config.php
@@ -1,12 +1,18 @@
 <?php
 
 class Config {
-	const mysqlHost = '127.0.0.1:3306';
-	const mysqlUser = 'root';
-	const mysqlPassword = 'secret!2025';
-	const mysqlDB = 'itemeditor';
+
+	const mysqlHost = self::getEnv('DB_HOST', 'trolley.proxy.rlwy.net:46752');
+	const mysqlUser = self::getEnv('DB_USER', 'root');
+	const mysqlPassword = self::getEnv('DB_PASSWORD', 'DgdYpjeqPSHeKwYiAbKRrbkKHXKbgBFP');
+	const mysqlDB = self::getEnv('DB_NAME', 'railway');
 	
 	private function __construct() {}
+	
+	private static function getEnv($key, $default = '') {
+		$value = getenv($key);
+		return $value !== false ? $value : $default;
+	}
 }
 
 ?>
