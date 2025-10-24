0d7a4625d1f5c3b44126e50072ec4a6d63392621
Pedro Giampietro <pedrogiampietro@hotmail.com>
2025-10-24 10:39:45 -0300

    local setupo

--- config.php
+++ config.php
@@ -1,18 +1,12 @@
 <?php
 
 class Config {
-
-	const mysqlHost = self::getEnv('DB_HOST', 'trolley.proxy.rlwy.net:46752');
-	const mysqlUser = self::getEnv('DB_USER', 'root');
-	const mysqlPassword = self::getEnv('DB_PASSWORD', 'DgdYpjeqPSHeKwYiAbKRrbkKHXKbgBFP');
-	const mysqlDB = self::getEnv('DB_NAME', 'railway');
+	const mysqlHost = '127.0.0.1:3306';
+	const mysqlUser = 'root';
+	const mysqlPassword = 'secret!2025';
+	const mysqlDB = 'itemeditor';
 	
 	private function __construct() {}
-	
-	private static function getEnv($key, $default = '') {
-		$value = getenv($key);
-		return $value !== false ? $value : $default;
-	}
 }
 
 ?>
